236cce3623e2c7c0e1638574973816e9fbf4fef5
anakanishi15 <anakanishi15@punahou.edu>
2014-03-07 14:21:35 -1000

    3/7/14
    Wouldn't let me sync without putting these in too.

--- Morse.java
+++ Morse.java
@@ -1,15 +1,17 @@
 //Amanda Nakanishi
 //2/21/14
-//Morse Code. Why? I got bored
+//Morse Code. I got bored
 
-import java.util.Scanner;
+import java.util.*;//means import everything from java utilities, which is what * means
+import java.io.*;
 
 public class Morse
 {
   private String message = "";//instance variable
   private String fileName = "";
   private String portion = "";//for taking portion without space.
-  p
+  private String print = "";//to save message final to.
+  
   
   public Morse(String newmessage, String name)
   {
@@ -17,28 +19,46 @@ public class Morse
     fileName = name;
   }
   
-  public void setUp()//sets up morse code
+  public void setUp()throws IOException//sets up morse code 
   {
+    String[] english = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};//declares array with every letter
     while(message.length() > 0)//while there's still characters in the message
     {
       for(int i = 0; i<message.length()-1; i++)//parse(?) 
       {
         if(message.substring(i,i+1).equals(" "))//if space, stop loop.
         {
-          portion = message.substring(0,i-1);//takes the part before the space.
-          message = message.substring(i, message.length());//takes rest of the message minus the space.
+          portion = message.substring(0,i);//takes the part before the space.
+          message = message.substring(i+1, message.length());//takes rest of the message minus the space.
+        System.out.println(portion);
+        System.out.println(message);
         }
-        match(portion);
+        int morseIndex = match(portion);
+        String character = english[morseIndex];
+        print += character; 
       }
     }
+    File end = new File(fileName + "Morse.txt");//name of file
+    FileWriter wr = new FileWriter(end);//yay internet. Writes text to file. This gets the file in question
+    wr.write(print);//writes message to file. REWRITES ANY EXISTING TEXT. BE CAREFUL.
+    wr.close();//closes
   }
   
-  public String match(String portion)//takes letter, matches it to grid.
+  public int match(String str)//takes letter, matches it to grid.
   {
-    String[] english = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};//declares array with every letter
+    portion = str; 
     String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};// international morse code A-Z
-    
-    String letter = "";//final returned letter. match it with original sent through line?
-    return letter;
+    for(int i = 0; i<26; i++)
+    {
+      if(morse[i].equals(portion))
+      {
+        //System.out.println(i);
+        return i;
+      }
+    }
+    return 0;//won't ever need a return statement, cause will get no matter what. What do you put there?
   }
 }
+
+//right now it can separate the character correctly, it gets rid of the spaces and delete the character from the message for the first one, but after that, it screws up.
+// for the first separated character, it works fine. After that, it's bad.
